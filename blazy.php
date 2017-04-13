81c4e1cf2b1107e356c3bf260de7b7a87b800b6a
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-04-13 15:58:53 -0600

    Update blazy.php

--- blazy.php
+++ blazy.php
@@ -8,7 +8,25 @@
   Author URI: https://github.com/lukecav
  */
  
- //Code Stuff Soon
+if ( ! class_exists( 'BlazyLoad_Images' ) ) :
+class BlazyLoad_Images {
+	const version = '1.0';
+	protected static $enabled = true;
+	static function init() {
+		if ( is_admin() )
+			return;
+		if ( ! apply_filters( 'blazyload_is_enabled', true ) ) {
+			self::$enabled = false;
+			return;
+		}
+		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'add_scripts' ) );
+		add_action( 'wp_head', array( __CLASS__, 'setup_filters' ), 9999 ); // we don't really want to modify anything in <head> since it's mostly all metadata, e.g. OG tags
+	}
+	static function setup_filters() {
+		add_filter( 'the_content', array( __CLASS__, 'add_image_placeholders' ), 99 ); // run this later, so other content filters have run, including image_add_wh on WP.com
+		add_filter( 'post_thumbnail_html', array( __CLASS__, 'add_image_placeholders' ), 11 );
+		add_filter( 'get_avatar', array( __CLASS__, 'add_image_placeholders' ), 11 );
+	}
 
 static function add_scripts() {
 		wp_enqueue_script( 'blazy',  self::get_url( 'js/blazy.js' ), array( 'blazy' ), self::version, true );
