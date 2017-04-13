0892044fdfe3cf342aadc9ac00a6a25a92ade5e9
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-04-13 15:51:09 -0600

    Update blazy.php

--- blazy.php
+++ blazy.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: WP Blazy
   Plugin URI: https://github.com/lukecav/wp-blazy
-  Description: WordPress plugin which uses blazy.js for lazy loading images on a site.
+  Description: WordPress plugin which uses bLazy.js for lazy loading images on a site.
   Author: Luke Cavanagh
   Version: 1.0
   Author URI: https://github.com/lukecav
